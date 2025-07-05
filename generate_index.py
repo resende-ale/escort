3e1338ab63a16c28ffa903f271e5851446b608cd
ale-the-creator <alexandrerefer@gmail.com>
2025-07-05 02:58:23 -0300

    fix

--- generate_index.py
+++ generate_index.py
@@ -10,7 +10,7 @@ html_start = '''<!DOCTYPE html>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Carros à Venda</title>
-  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
   <header>
